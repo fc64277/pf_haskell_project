a24a45707d1e457fd46a97148e5ea5506f097f9c
Diogo Borges <fc64277@alunos.ciencias.ulisboa.pt>
2026-05-08 09:28:30 +0100

    training func with description

--- projecto.hs
+++ projecto.hs
@@ -129,4 +129,16 @@ backPropagation eta input expOut net =
 
 -- | Treina a rede durante n iterações, com uma taxa de
 -- aprendizagem e percorrendo os exemplos ciclicamente.
--- training :: Int -> Double -> ([[Double]], [[Double]]) -> Network -> Network
+training :: Int -> Double -> ([[Double]], [[Double]]) -> Network -> Network
+
+training 0 _ _ net = net
+training it eta (inputMtx, expOutputs) net = 
+  let
+    -- emparelhar input com output
+    dataset = zip inputMtx expOutputs
+    -- para cada valor do dataset, foldl vai aplicar uma função lambda
+    -- esta tem parâmetros currentNet (net) e dataset com PatternMatching em (input, output)
+    -- dataset faz backPropagation pelos inputs e outputs
+    newNet = foldl (\currentNet (input, output) -> backPropagation eta input output currentNet) net dataset
+  -- nova iteração
+  in training (it - 1) eta (inputMtx, expOutputs) newNet
